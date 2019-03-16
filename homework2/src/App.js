3914ac73d4c881afc710930694ddbdfce0df3eb5
Steve Tijerina <sntijerina@gmail.com>
2019-03-16 10:40:38 -0500

    Initial commit of Homework2
    Commit Homework2

---
+++ homework2/src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class CurrentDateTime extends Component {
+  
+  getCurrentDateTime = ()=> { return new Date().toLocaleString(); };
+
+  render() {
+    return (
+      <p>Current Time: {this.getCurrentDateTime()}</p>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            My First React App!
+          </p>
+          
+          <CurrentDateTime></CurrentDateTime>
+          
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
