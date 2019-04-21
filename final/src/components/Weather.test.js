f15d4e77ffa2bd246ad70f7a1ddbb3c7888b6be1
Steve Tijerina <sntijerina@gmail.com>
2019-04-21 08:17:47 -0500

    add initial unit tests

--- final/src/components/Weather.test.js
+++ final/src/components/Weather.test.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import Enzyme, { shallow, mount } from 'enzyme';
-import Weather from "./Weather";
-import WeatherForm from "./WeatherForm";
-import WeatherCard from "./WeatherCard";
+import Enzyme, { shallow } from 'enzyme';
+import Weather from './Weather';
 import Adapter from "enzyme-adapter-react-16";
-import { createShallow } from '@material-ui/test-utils';
-
 
 Enzyme.configure({ adapter: new Adapter() });
 
-jest.mock("../api/GetWeatherApi");
-
-describe("Weather component", () => {
-    let shallow;
-    beforeAll(() => {
-        shallow = createShallow({ dive: true });
-    });
-
-    it("renders", () => {
-        const wrapper = shallow(<Weather />);
-
-        expect(wrapper.exists()).toBe(true);
-    });
-
+describe('Weather', () => {
+  it('renders', () => {
+    const component = shallow(<Weather />);
+  
+    expect(component).toMatchSnapshot();
+  });
 });
